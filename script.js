8c3f200bff2dc3906e669d0c4170ea2c15a28e2d
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-09 07:45:49 +0900

    object追加！(java)

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function changeMessage() {
     document.getElementById("msg").
     textContent= "ボタンが押されたよ！";}
+
 function showAlert() {alert("こんにちは！");}
+
 console.log("javascriptが動いています！");
+
+const fruits = ["りんご","バナナ","みかん"];
+console.log(fruits[0]); //りんご
+console.log(fruits[2]); //みかん
+
+const person = {name:"轟三郎", age: x , hobby: "x"};
+console.log(person.name); 
+console.log(person.hobby); 
+
+function showData() {document.getElementById("dataArea").textContent =
+    person.name + "(趣味：" + person.hobby +") " ; }

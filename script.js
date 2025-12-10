f04d84410bac1b296921633c81f1d928e50245e1
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-10 14:25:54 +0900

    js修正

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ function changeColor()
 
 document.getElementById("eventBtn").addEventListener("click",function() 
     {document.getElementById("eventMsg").textContent = "イベントで変わったよ！"; });
+
+// ボタンを取得してイベントを登録
+document.getElementById("sendButton").addEventListener("click", sendName);
+
+// 名前を FastAPI に送る関数
+async function sendName() {
+    const nameValue = document.getElementById("nameInput").value;
+
+    const response = await fetch("http://127.0.0.1:8000/hello-name", {
+        method: "POST",
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify({ name: nameValue })
+    });
+
+    const data = await response.json();
+    document.getElementById("apiResult").textContent = data.message;
+}

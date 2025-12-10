fd195564bd83678f1fc700e2f8368f648253782d
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-10 14:30:02 +0900

    html修正

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/hello")
 def hello():
     return {"message": "こんにちは！FastAPI だよ！"}
+
+class NameRequest(BaseModel):
+    name: str
+
+@app.post("/hello-name")
+def hello_name(data: NameRequest):
+    return {"message": f"こんにちは、{data.name}さん！"}

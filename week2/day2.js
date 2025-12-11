c9fefb307006764940c13123d3fdc1173354e273
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-11 20:48:12 +0900

    js追加

--- week2/day2.js
+++ week2/day2.js
@@ -5,3 +5,19 @@ function sayHello(){
 
 // ② 関数の呼び出し
 sayHello();
+
+
+function introduce(name,age){
+    const message = `${name}は${age}歳です`;
+    return message;
+}
+
+console.log(introduce("轟三郎",28));
+
+
+function introduce2(name, age) {
+    return `${name}さんは${age}歳です`;
+}
+
+const message2 = introduce2("たけし", 30);
+console.log(message2);

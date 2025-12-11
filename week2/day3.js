3833546ee7386f8768a46685ad95e359b54fa706
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-11 23:26:20 +0900

    characters追加

--- week2/day3.js
+++ week2/day3.js
@@ -21,3 +21,13 @@ console.log(hero)
 for (let i = 0; i < fruits.length; i++) {
     console.log(fruits[i]);
 }
+
+const characters = [
+    createCharacter("轟三郎", 100, 30),
+    createCharacter("たけし", 80, 20),
+    createCharacter("さぶろう", 120, 40),
+];
+
+for (let i = 0; i < characters.length; i++) {
+    console.log(characters[i].name + " の攻撃力：" + characters[i].attack);
+}

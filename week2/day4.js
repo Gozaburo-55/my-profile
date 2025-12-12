f1ea589ce7f6aaf35295b1ac1234fda51b04ccb6
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-12 22:45:07 +0900

    書いておきたいこと

--- week2/day4.js
+++ week2/day4.js
@@ -7,15 +7,12 @@ btn.addEventListener("click",function(){
     msg.textContent = "ボタンが押されたよ！"
 });
 
-let isRed = false; 
-btn.addEventListener("click", function(){ 
-    if(isRed){
-        msg.style.color = "black";
-        isRed = false; 
-    } else{ msg.style.color = "red"; 
-        isRed = true; 
-    } 
-})
+//let isRed = false; 
+// btn.addEventListener("click", function(){ 
+// if(isRed){msg.style.color = "black";
+// isRed = false; 
+// } else{ msg.style.color = "red"; 
+// isRed = true; } })
 
 const secret = document.getElementById("secret");
 const toggle = document.getElementById("toggle");

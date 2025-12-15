33f2aa966728a7540a13adc7f747166335de8d20
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-15 16:49:41 +0900

    day6 js start

---
+++ day6.js
@@ -0,0 +1,19 @@
+const sendBtn = document.getElementById("sendBtn");
+const input = document.getElementById("inputName");
+const result = document.getElementById("result");
+
+
+sendBtn.addEventListener("click",async function(){
+    const name = input.value;
+
+    const response = await fetch("http/127.0.0.1:8000/hello",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({name:name})
+    });
+    const data = await response.json();
+    result.textContent = data.message;
+});
+

617f799507760f01f1d04fd4c2792f7ba7ab19cb
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-16 22:08:07 +0900

    js

---
+++ week3/day1.js
@@ -0,0 +1,24 @@
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const sendBtn = document.getElementById("sendBtn");
+const result = document.getElementById("result");
+
+sendBtn.addEventListener("click",async function(){
+    const name = nameInput.Value
+    const job = jobInput.Value
+
+    if(!name||!job){
+        result.textContent = "名前・職業を入力してね！";
+        return
+    }
+
+    const response = await fetch ("http://127.0.0.1:8000/profile",{
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({name:name,job:job}),
+    });
+
+    const data = await response.json();
+    result.textContent = data.message;
+});
+

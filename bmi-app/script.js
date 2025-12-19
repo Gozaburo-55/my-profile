13b96044ce142d23fb0601ebcb0b65125150c1e1
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-19 17:54:04 +0900

    bmiText

--- bmi-app/script.js
+++ bmi-app/script.js
@@ -3,10 +3,8 @@ const heightInput = document.getElementById("heightInput")
 const kgInput = document.getElementById("kgInput")
 const calcBtn =document.getElementById("calcBtn")
 const BMIresult = document.getElementById("BMIresult")
-let BMIjudge = document.getElementById("BMIjudge")
+const BMIjudge = document.getElementById("BMIjudge")
 const weightresult = document.getElementById("weightresult")
-
-
 
 //ボタンが押されたときの処理
 calcBtn.addEventListener("click", function () {
@@ -20,20 +18,26 @@ calcBtn.addEventListener("click", function () {
     const weightT = (heightM*heightM)*22
     const bmi = kg/(heightM*heightM)
 
+    //判定情報
+    let bmiText = "";
+    
+    if (BMIresult < 18.5){
+        bmiText = "痩せ（BMI<18.5";}
+    else if (BMIresult < 25){
+        bmiText = "標準（18.5≦BMI<25）"}
+    else if (BMIresult < 30){
+        bmiText = "肥満（25≦BMI<30）"}
+    else if (BMIresult >= 30){
+        bmiText = "高度肥満（30≦BMI）"}
+
     //結果出力
     weightresult.textContent = 
     `あなたの適正体重は${weightT.toFixed(1)}kgです！`; //適正体重
     BMIresult.textContent = 
     `あなたのBMIは${bmi.toFixed(1)}です！`; //BMI
-
-    if (BMIresult << 18.5){
-        BMIjudge.textContent = "痩せ（BMI<18.5";}
-    else if (BMIresult << 25){
-        BMIjudge.textContent = "標準（18.5≦BMI<25）"}
-    else if (BMIresult << 30){
-        BMIjudge.textContent = "肥満（25≦BMI<30）"}
-    else if (BMIresult >= 30){
-        BMIjudge.textContent = "高度肥満（30≦BMI）"}
+    BMIjudge.textContent = 
+    `判定：${bmiText}`
+     
 });
 
 

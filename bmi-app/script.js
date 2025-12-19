a276446babca8189f55405feee8bda93867a846b
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-19 16:25:50 +0900

    calc js

--- bmi-app/script.js
+++ bmi-app/script.js
@@ -1,6 +1,6 @@
 //htmlから変数を引き出す
-const Inputheight = document.getElementById("inputHeight")
-const Inputkg = document.getElementById("inputKg")
+const heightInput = document.getElementById("heightInput")
+const kgInput = document.getElementById("kgInput")
 const calcBtn =document.getElementById("calcBtn")
 const result = document.getElementById("result")
 
@@ -8,7 +8,13 @@ const result = document.getElementById("result")
 
 //ボタンが押されたときの処理
 calcBtn.addEventListener("click", function () {
-  result.textContent = "ボタンが押された！";
+    const height = Number(heightInput.value)//cm
+    const kg = Number(kgInput.value)//kg
+
+    const heightM = height/100
+    const calcresult = kg/heightM^2
+
+    result.textContent = `あなたのBMIは${calcresult}です！`;
 });
 
 

fbea892e562abc9339ea629bae8d966ab2684efe
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-19 16:54:59 +0900

    weight T add

--- bmi-app/script.js
+++ bmi-app/script.js
@@ -2,19 +2,26 @@
 const heightInput = document.getElementById("heightInput")
 const kgInput = document.getElementById("kgInput")
 const calcBtn =document.getElementById("calcBtn")
-const result = document.getElementById("result")
+const BMIresult = document.getElementById("BMIresult")
+const weightresult = document.getElementById("weightresult")
 
 
 
 //ボタンが押されたときの処理
 calcBtn.addEventListener("click", function () {
+
+    //計算情報
     const height = Number(heightInput.value)//cm
     const kg = Number(kgInput.value)//kg
 
     const heightM = height/100
+
+    const weightT = (heightM*heightM)*22
     const bmi = kg/(heightM*heightM)
 
-    result.textContent = `あなたのBMIは${bmi.toFixed(1)}です！`;
+    //結果出力
+    weightresult.textContent = `あなたの適正体重は${weightT.toFixed(1)}です！`; //適正体重
+    BMIresult.textContent = `あなたのBMIは${bmi.toFixed(1)}です！`; //BMI
 });
 
 

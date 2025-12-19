6820e12c32ed5b4ea68685f391ae418530dc165e
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-19 16:07:24 +0900

    bmi app html

---
+++ bmi-app/script.js
@@ -0,0 +1,19 @@
+//htmlから変数を引き出す
+const Inputheight = document.getElementById("inputHeight")
+const Inputkg = document.getElementById("inputKg")
+const calcBtn =document.getElementById("calcBtn")
+const result = document.getElementById("result")
+
+
+
+//ボタンが押されたときの処理
+calcBtn.addEventListener("click",functon(){
+    const height = inputHeight.value;
+
+    const kg = inputKg.value;
+
+    const calcresult = 
+})
+
+
+

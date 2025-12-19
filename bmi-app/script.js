39a3a19fbfd5c7a354649deef0981ad0ce0c8163
Gozaburo-55 <gggyh31256@gmail.com>
2025-12-19 21:15:01 +0900

    color js

--- bmi-app/script.js
+++ bmi-app/script.js
@@ -22,14 +22,26 @@ calcBtn.addEventListener("click", function () {
     let bmiText = "";
     
     if (bmi < 18.5){
-        bmiText = "痩せ（BMI<18.5";}
+        bmiText = "痩せ（BMI<18.5)";
+    bmiText.style.color = "blue"}
     else if (bmi < 25){
-        bmiText = "標準（18.5≦BMI<25）"}
+        bmiText = "標準（18.5≦BMI<25）";
+    bmiText.style.color = "green"}
     else if (bmi < 30){
-        bmiText = "肥満（25≦BMI<30）"}
+        bmiText = "肥満（25≦BMI<30）";
+    bmiText.style.color = "orange"}
     else if (bmi >= 30){
-        bmiText = "高度肥満（30≦BMI）"}
+        bmiText = "高度肥満（30≦BMI）";
+    bmiText.style.color = "red"}
+    
+    //身長・体重が入力されていなかった場合
+    if (!height ||!kg){
+        BMIjudge.textContent="身長と体重を入力してください！";
+        bmiText.style.color = "red"
+        return;
+    }
 
+    
     //結果出力
     weightresult.textContent = 
     `あなたの適正体重は${weightT.toFixed(1)}kgです！`; //適正体重
